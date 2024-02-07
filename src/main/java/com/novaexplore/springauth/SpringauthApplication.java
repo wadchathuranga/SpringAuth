fbae5fd906477604daf1c49eaa36a29761b30750
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-02-07 13:40:25 +0530

    initial commit

---
+++ src/main/java/com/novaexplore/springauth/SpringauthApplication.java
@@ -0,0 +1,13 @@
+package com.novaexplore.springauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringauthApplication.class, args);
+	}
+
+}
